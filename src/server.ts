34232346df75f6641e1fdde69ba75a2c3e17a961
Chatchawan <chatchawan_t@fissionsoft.co.th>
2025-12-27 12:51:53 +0700

    api

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import sensorRoutes from './routes/sensor.routes';
+import sensorRoutes from './routes/sensor.routes.js';
 
 dotenv.config();
 
