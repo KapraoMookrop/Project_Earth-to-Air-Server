792cd4019120b9ae18a3c5aadf71bced49eb76e3
Chatchawan <chatchawan_t@fissionsoft.co.th>
2025-12-27 13:24:35 +0700

    type data

---
+++ src/types/SettingsData.ts
@@ -0,0 +1,8 @@
+export interface SettingsData {
+  device_id: number;
+  is_auto_mode: boolean;
+  wind_speed_baseline: number;
+  filter_alert_threshold: number;
+  line_notify_token: string;
+  electricity_rate: number;
+}
